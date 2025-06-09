214d8072f18b541623db33c202e40fa76d38406c
YunnDian, Wang <yunjen1225@gmail.com>
2025-06-09 14:38:33 -0700

    update swiper setting
    更新輪播設定、CSS樣式設定

--- js/swiper_setting.js
+++ js/swiper_setting.js
@@ -1,19 +1,47 @@
-var swiper = new Swiper(".mySwiper", {
-  direction: "vertical",
-  loop: true,
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper2 = new Swiper(".mySwiper2", {
-  loop: true,
-  spaceBetween: 10,
+var swiper = new Swiper(".projectSwiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  thumbs: {
-    swiper: swiper,
+  loop: true,
+  speed: 2700,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
+  autoplay: {
+    delay: 2000,
+    pauseOnMouseEnter: true,
+    disableOnInteraction: false,
+  },
+});
+var swiper = new Swiper(".threePicSwiper", {
+  slidesPerView : 1,
+  slidesPerGroup : 1,
+  spaceBetween: 30,
+  freeMode: true,
+  loop: true,
+  speed: 2700,
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    992: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 800,
+    pauseOnMouseEnter: true,
+    disableOnInteraction: false,
+  },
+});
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
 });
