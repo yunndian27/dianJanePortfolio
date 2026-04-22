b22b88e27c1b86797a07a2aac22bda98b13601a1
YunnDian, Wang <yunjen1225@gmail.com>
2026-04-22 14:34:06 +0800

    finish home, add about - 20260422
    完成首頁，加寫關於中...

--- js/swiper_setting.js
+++ js/swiper_setting.js
@@ -1,47 +1,77 @@
-var swiper = new Swiper(".projectSwiper", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+var swiper = new Swiper(".otherSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  lazy: true,
   loop: true,
-  speed: 2700,
+  autoplay: {
+    delay: 2500,
+    pauseOnMouseEnter: true,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-  autoplay: {
-    delay: 2000,
-    pauseOnMouseEnter: true,
-    disableOnInteraction: false,
-  },
-});
-var swiper = new Swiper(".threePicSwiper", {
-  slidesPerView : 1,
-  slidesPerGroup : 1,
-  spaceBetween: 30,
-  freeMode: true,
-  loop: true,
-  speed: 2700,
   breakpoints: {
     576: {
       slidesPerView: 2,
-      slidesPerGroup: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
     },
     992: {
       slidesPerView: 3,
-      slidesPerGroup: 3,
+      spaceBetween: 40,
     },
   },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  autoplay: {
-    delay: 800,
-    pauseOnMouseEnter: true,
-    disableOnInteraction: false,
-  },
 });
-Fancybox.bind("[data-fancybox]", {
-  // Your custom options
-});
+
+// var swiper = new Swiper(".projectSwiper", {
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   loop: true,
+//   speed: 2700,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   autoplay: {
+//     delay: 2000,
+//     pauseOnMouseEnter: true,
+//     disableOnInteraction: false,
+//   },
+// });
+// var swiper = new Swiper(".threePicSwiper", {
+//   slidesPerView : 1,
+//   slidesPerGroup : 1,
+//   spaceBetween: 30,
+//   freeMode: true,
+//   loop: true,
+//   speed: 2700,
+//   breakpoints: {
+//     576: {
+//       slidesPerView: 2,
+//       slidesPerGroup: 2,
+//     },
+//     992: {
+//       slidesPerView: 3,
+//       slidesPerGroup: 3,
+//     },
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   autoplay: {
+//     delay: 800,
+//     pauseOnMouseEnter: true,
+//     disableOnInteraction: false,
+//   },
+// });
+// Fancybox.bind("[data-fancybox]", {
+//   // Your custom options
+// });
